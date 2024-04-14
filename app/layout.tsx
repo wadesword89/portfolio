500732c8968606bae9918184a4a59b991fdd6e34
Prashay <113265973+wadesword89@users.noreply.github.com>
2024-04-14 16:07:37 -0700

    add background

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Prashay's Portfolio Site",
-  description: "Prashay's portfolio site",
+  title: "Prashay | Personal Portfolio",
+  description: "Prashay is a full-stack developer",
 };
 
 export default function RootLayout({
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased bg-gray-50 text-gray-950 relative`}>
+        {/* Background Blobs:
+          - The blobs are created using divs with absolute positioning. Blobs are circular divs with a blur filter applied to them.
+          - The -z-10 is used to place the blobs behind the body content
+        */}
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-slate-500">
+    <main>
       TEST
     </main>
   );
