6311be4437929e7084a698c8e0200eb5a1d8c8dc
Prashay <113265973+wadesword89@users.noreply.github.com>
2024-04-15 19:54:46 -0700

    add header styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,6 +23,8 @@ export default function RootLayout({
         */}
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
+
         {children}
       </body>
     </html>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,22 @@
+'use client'; //needed since we are using motion.div
+
 import React from 'react'
+import { motion } from 'framer-motion';
 
 export default function Header() {
   return (
-    <header className=''>Header</header>
+    <header className=''>
+      {/* motion.div is used to animate the header  */}
+      <motion.div 
+        // top-0 is used to place the header at the top of the page, 
+        // z-50 is used to place the header above the blobs
+        className='bg-white w-1/2 h-16 shadow-md fixed border rounded-full top-0 z-50'
+        //initial state of the header.
+        initial={{ y: -100, opacity: 0 }} //y: -100 moves the header 100px up, opacity: 0 makes the header invisible
+        //animate the header 
+        animate={{ y: 0, opacity: 1 }} //y: 0 moves the header to its original position, opacity: 1 makes the header visible
+      >
+      </motion.div>
+    </header>
   )
 }
