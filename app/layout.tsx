0bb8c2d5b6e8184e6a69a100086371ec51f73577
Prashay <113265973+wadesword89@users.noreply.github.com>
2024-04-19 15:49:52 -0700

    add nav links

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
         <Header />
 
         {children}
+        
       </body>
     </html>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,36 @@
-'use client'; //needed since we are using motion.div
+"use client";
 
-import React from 'react'
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
+import { links } from "@/lib/data";
+import Link from "next/link";
+import clsx from "clsx";
 
 export default function Header() {
+
+
   return (
-    <header className=''>
-      {/* motion.div is used to animate the header  */}
-      <motion.div 
-        // top-0 is used to place the header at the top of the page, 
-        // z-50 is used to place the header above the blobs
-        className='bg-white w-1/2 h-16 shadow-md fixed border rounded-full top-0 z-50'
-        //initial state of the header.
-        initial={{ y: -100, opacity: 0 }} //y: -100 moves the header 100px up, opacity: 0 makes the header invisible
-        //animate the header 
-        animate={{ y: 0, opacity: 1 }} //y: 0 moves the header to its original position, opacity: 1 makes the header visible
-      >
-      </motion.div>
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      ></motion.div>
+
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link href={link.hash}>{link.name}</Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
