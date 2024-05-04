819471e82c1d2ce22df9d37d81cc37312eba88e2
Prashay <113265973+wadesword89@users.noreply.github.com>
2024-05-04 14:21:42 -0700

    add description:

--- components/About.tsx
+++ components/About.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='mb-28 text-center leading-8 max-w-[45rem] sm:mb-40'>
+      <h2 className='text-3xl font-medium mb-8 mt-11 capitalize'>About Me</h2>
+
+      <p className="mb-3">
+        I specialize in building full stack TypeScript/JavaScript applications with Node/Express, React/Redux, and SQL/NoSQL. I co-developed Ludwig VS, a tool that detects web-accessibility violations in real-time as a developer codes in an IDE.
+      </p>
+
+      <p className=''>
+        When I'm not coding, I enjoy creating art, working out, and playing sports like basketball and tennis. I am also currently pursuing a Masters of Science in Computer Science from the Georgia Institute of Technology.
+      </p>
+    </section>
   )
 }
 
